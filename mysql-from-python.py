44f454de6f57b8b846478478e6382f47f90d80e6
michaelhesch <michael.hesch@gmail.com>
2021-08-03 12:43:41 +0000

    add python file to connect to test sql database

---
+++ mysql-from-python.py
@@ -0,0 +1,24 @@
+import os
+import pymysql
+
+# Get username from workspace
+# (modify if running on another environment)
+username = os.getenv('GITPOD_USER_NAME')
+
+# Connect to the database
+connection = pymysql.connect(host='localhost',
+                            user=username,
+                            password='',
+                            db='Chinook')
+
+try:
+    # Run a query 
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM Artist;"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print(result)
+finally:
+    # close the db connection, regardless of what happens
+    connection.close()
+
